45f93cae37ac9ef4d253b334f1b0eabfcfb3a0db
Evgeniy Azarov <azarov.private@gmail.com>
2022-10-22 05:46:43 +0300

    Add avatar component

---
+++ src/components/avatar.tsx
@@ -0,0 +1,15 @@
+interface AvatarProps {
+  className?: string
+}
+
+export const Avatar = ({className}: AvatarProps) => {
+  return (
+    <div className={`${className} h-40 w-40 flex-none rounded-3xl`}>
+      <img
+        className="rounded-3xl"
+        src="https://avatars.githubusercontent.com/u/7345847?v=4"
+        alt="avatar"
+      />
+    </div>
+  )
+}
